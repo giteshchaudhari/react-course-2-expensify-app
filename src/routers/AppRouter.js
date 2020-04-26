af65fec3a443b5bc1ceec3802defbef77888bb3d
gitesh chaudhari <gpchaudhari1999@gmail.com>
2020-04-26 11:26:34 +0530

    last commit hopefulyy

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,7 +7,7 @@ import HelpPage  from './../component/HelpPage';
 import EditExpensePage from './../component/EditExpensePage';
 import NotFoundPage from './../component/NotFoundPage';
 import LoginPage from './../component/LoginPage';
-//import {PrivateRoute} from './PrivateRoute';
+import {PrivateRoute} from './PrivateRoute';
 import {PublicRoute} from './PublicRoute';
 
 export const history=createHistory();
